5e956780218ff69cbc9b2d1180530ba4f96eef96
Mônica Nascimento <60589987+M-Hyuga@users.noreply.github.com>
2023-01-02 06:04:36 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+/*
+ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INICIO
+var c = 1
+while (c <= 5 ) {
+    console.log (`Passo ${c}`)
+    c++
+} */
+
+/*ESTRUTURA DE REPETIÇÃOCOM TESTE LÓGICO NO FIM */
+var c = 1
+do {
+    console.log (`Passo ${c}`)
+    c++
+} while (c <=4)
